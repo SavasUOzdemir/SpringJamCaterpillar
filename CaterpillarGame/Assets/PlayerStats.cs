fc363a14f479738508a7887d15da704300f49fc5
SavasUOzdemir <94976406+SavasUOzdemir@users.noreply.github.com>
2023-05-07 01:05:03 +0300

    add stuff i did in marmaray
    add playerstats object

--- CaterpillarGame/Assets/PlayerStats.cs
+++ CaterpillarGame/Assets/PlayerStats.cs
@@ -8,21 +8,54 @@ public class PlayerStats : MonoBehaviour
     public bool _InDangerZone { get; set; }
     [SerializeField] private float _waitSeconds = 1;
     [SerializeField] private float _dangerZoneTickingDamage;
+    private Coroutine _tickingCoroutineReference;
+
+    const string SAFEZONETAG = "SafeZone";
     public void DangerMeterChange(float dangermeterchange)
     {
         _DangerMeter += dangermeterchange;
         return;
     }
 
-    private void Die() 
+    private void Start()
     {
-        
+        _tickingCoroutineReference = StartCoroutine(DamageTick());
+    }
+
+    private void Die()
+    {
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.CompareTag(SAFEZONETAG))
+            return;
+        _InDangerZone = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (!other.CompareTag(SAFEZONETAG) || !_InDangerZone)
+            return;
+        _InDangerZone = false;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!other.CompareTag(SAFEZONETAG))
+            return;
+        _InDangerZone = true;
     }
 
     IEnumerator DamageTick()
     {
         while (true)
         {
+            while (!_InDangerZone) 
+            { 
+                yield return new WaitForSeconds(_waitSeconds);
+            }
             yield return new WaitForSeconds(_waitSeconds);
             DangerMeterChange(_dangerZoneTickingDamage);
         }
